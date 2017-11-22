626501412d541edad6fb49aa2a82a73e500f2805
wngrahams <wgs11@georgetown.edu>
2017-11-22 12:04:38 -0500

    Game pieces can be drawn on board

--- src/client/GameBoardPanel.java
+++ src/client/GameBoardPanel.java
@@ -9,32 +9,31 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GameBoardPanel extends JPanel implements MouseListener{
+	
+	private static final int EMPTY = 0b00;
+	private static final int WHITE = 0b01;
+	private static final int BLACK = 0b10;
+	
+	private int[][] gameState = new int[15][15];
 
 	public GameBoardPanel() {
 		setDoubleBuffered(true);
 		
 		initializePanel();
+		resetBoard();
 	}
-
+	
 	private void initializePanel() {
 		setBackground(Color.WHITE);
 		
 		addMouseListener(this);
 	}
 	
-	public void paint(Graphics g){
-		// draw background
-		Color backgroundColor = new Color(0.9f, 0.7f, 0.2f, 0.35f);
-		g.setColor(backgroundColor); 
-		int w = getWidth();  
-		int h = getHeight();
-		g.fillRect( 0, 0, w, h ); 
-		
-		// draw lines
-		g.setColor(Color.BLACK);
-		for (int i=1; i<16; i++) {
-			g.drawLine((int)(w/16.0)*i, 0, (int)(w/16.0)*i, h);
-			g.drawLine(0, (int)(h/16.0)*i, w, (int)(h/16.0)*i);
+	public void resetBoard() {
+		for (int i=0; i<15; i++) {
+			for (int j=0; j<15; j++) {
+				gameState[i][j] = EMPTY;
+			}
 		}
 	}
 	
@@ -55,4 +54,37 @@ public class GameBoardPanel extends JPanel implements MouseListener{
 
 	@Override
 	public void mouseExited(MouseEvent e) {}
+	
+	@Override
+	public void paint(Graphics g){
+		// draw background
+		Color backgroundColor = new Color(0.9f, 0.7f, 0.2f, 0.35f);
+		g.setColor(backgroundColor); 
+		int w = getWidth();  
+		int h = getHeight();
+		g.fillRect( 0, 0, w, h ); 
+		
+		// draw lines
+		g.setColor(Color.BLACK);
+		for (int i=1; i<16; i++) {
+			g.drawLine((int)(w/16.0)*i, 0, (int)(w/16.0)*i, h);
+			g.drawLine(0, (int)(h/16.0)*i, w, (int)(h/16.0)*i);
+		}
+		
+		// draw pieces played
+		// TODO: make this more efficient by just checking for updates, not searching whole board every time
+		for (int i=0; i<15; i++) {
+			for (int j=0; j<15; j++) {
+				if (gameState[i][j] != EMPTY) {
+					if (gameState[i][j] == WHITE)
+						g.setColor(Color.WHITE);
+					else if (gameState[i][j] == BLACK)
+						g.setColor(Color.BLACK);
+					
+					int diameter = Math.min((int)(w/16.0), (int)(h/16.0));
+					g.fillOval((int)(w/16.0)*(i+1) - diameter/2, (int)(h/16.0)*(j+1) - diameter/2, diameter, diameter);
+				}
+			}
+		}
+	}
 }
