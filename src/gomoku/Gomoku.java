781892121a50592a579b1ccb15c59fe1690a64af
wngrahams <wgs11@georgetown.edu>
2017-11-26 11:00:34 -0500

    Added method to return color ints as Strings

--- src/gomoku/Gomoku.java
+++ src/gomoku/Gomoku.java
@@ -4,4 +4,13 @@ public class Gomoku {
 	public static final int EMPTY = 0b11111111111111111111111111111111;
 	public static final int WHITE = 0b00;
 	public static final int BLACK = 0b01;
+	
+	public static String colorAsString(int color) {
+		if (color == WHITE)
+			return "White";
+		else if (color == BLACK)
+			return "Black";
+		else
+			return "";
+	}
 }
