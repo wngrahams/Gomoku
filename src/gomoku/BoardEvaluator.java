b8351da2d35bd97f3edc9b12d59a29e728e9d7e2
wngrahams <wgs11@georgetown.edu>
2017-12-04 16:45:14 -0500

    constructor

--- src/gomoku/BoardEvaluator.java
+++ src/gomoku/BoardEvaluator.java
@@ -4,8 +4,14 @@ import java.util.PriorityQueue;
 
 public class BoardEvaluator {
 	
+	private int[][] gameState;
+	
 	public BoardEvaluator(int[][] gameState) {
-		
+		for (int i=0; i<15; i++) {
+			for (int j=0; j<15; j++) {
+				this.gameState[i][j] = gameState[i][j];
+			}
+		}
 	}
 	
 	public PriorityQueue<GomokuMove> evaluateBoard(){
