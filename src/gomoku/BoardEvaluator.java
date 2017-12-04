a8e6ed526c65aaf3c13d4281970a8b00ac2702c2
wngrahams <wgs11@georgetown.edu>
2017-12-04 16:43:08 -0500

    Added BoardEvaluator class

---
+++ src/gomoku/BoardEvaluator.java
@@ -0,0 +1,14 @@
+package gomoku;
+
+import java.util.PriorityQueue;
+
+public class BoardEvaluator {
+	
+	public BoardEvaluator(int[][] gameState) {
+		
+	}
+	
+	public PriorityQueue<GomokuMove> evaluateBoard(){
+		
+	}
+}
